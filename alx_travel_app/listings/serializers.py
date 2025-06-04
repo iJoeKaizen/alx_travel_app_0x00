d2d3fcdc99d311937045d66f74104dbbf39e97cb
Gideon Mensah <142912117+iJoeKaizen@users.noreply.github.com>
2025-06-04 06:03:56 +0000

    Create serializers.py

---
+++ alx_travel_app/listings/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Listing, Booking
+
+class ListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = '__all__'
+        read_only_fields = ('owner', 'created_at', 'updated_at')
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+        read_only_fields = ('user', 'created_at', 'updated_at')
